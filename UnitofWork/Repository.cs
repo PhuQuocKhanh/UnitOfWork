640448cbffdc4c02d506b70239e2edbc9fbf0391
PhuQuocKhanh <87748809+PhuQuocKhanh@users.noreply.github.com>
2025-12-03 14:32:54 +0700

    UnitofWork Pattern

---
+++ UnitofWork/Repository.cs
@@ -0,0 +1,364 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+using UnitofWork.Helper;
+
+namespace UnitofWork
+{
+    public interface IRepository<T> where T : class
+    {
+        void ChangeTable(string table);
+        ValueTask<EntityEntry<T>> InsertAsync(T entity, CancellationToken cancellationToken = default(CancellationToken));
+        T Insert(T entity);
+        void Update(T entity);
+        void Attach(T entity);
+        public void UpdateFields(T entity, params Expression<Func<T, object>>[] updatedProperties);
+        public string GetPropertyName<TProperty>(Expression<Func<T, TProperty>> propertyExpression);
+
+        IQueryable<T> GetAll(Expression<Func<T, bool>>? predicate = null,
+                             Func<IQueryable<T>, IIncludableQueryable<IQueryable, object>>? include = null,
+                             Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                             bool disableTracking = false, bool IgnoreQueryFilter = false);
+
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? predicate = null,
+                                  Func<IQueryable<T>, IIncludableQueryable<IQueryable, object>>? include = null,
+                                  Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                  bool disableTracking = false, bool IgnoreQueryFilter = false);
+
+
+        TResult? GetFirstOrDefaultSelector<TResult>(Expression<Func<T, TResult>>? selector = null,
+                                           Expression<Func<T, bool>>? predicate = null,
+                                           Func<IQueryable<T>, IIncludableQueryable<IQueryable, object>>? include = null,
+                                           Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                           bool disableTracking = false, bool IgnoreQueryFilter = false);
+
+        Task<TResult?> GetFirstOrDefaultSelectorAsync<TResult>(Expression<Func<T, TResult>>? selector = null,
+                                                       Expression<Func<T, bool>>? predicate = null,
+                                                       Func<IQueryable<T>, IIncludableQueryable<IQueryable, object>>? include = null,
+                                                       Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                                       bool disableTracking = false, bool IgnoreQueryFilter = false);
+
+        T? GetFirstOrDefault(Expression<Func<T, bool>>? predicate = null,
+                             Func<IQueryable<T>, IIncludableQueryable<IQueryable, object>>? include = null,
+                             Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                             bool disableTracking = false, bool IgnoreQueryFilter = false);
+
+        Task<T?> GetFirstOrDefaultAsync(Expression<Func<T, bool>>? predicate = null,
+                                        Func<IQueryable<T>, IIncludableQueryable<IQueryable, object>>? include = null,
+                                        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                        bool disableTracking = false, bool IgnoreQueryFilter = false);
+
+        Task<PaginatedList<T>> GetToPageListAsync(Expression<Func<T, bool>>? predicate = null,
+                                                  Func<IQueryable<T>, IIncludableQueryable<IQueryable, object>>? include = null,
+                                                  Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                                  bool disableTracking = false, bool IgnoreQueryFilter = false,
+                                                  int pageSize = 10, int pageIndex = 1);
+
+        PaginatedList<T> GetToPageList(Expression<Func<T, bool>>? predicate = null,
+                                       Func<IQueryable<T>, IIncludableQueryable<IQueryable, object>>? include = null,
+                                       Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                       bool disableTracking = false, bool IgnoreQueryFilter = false,
+                                       int pageSize = 10, int pageIndex = 1);
+    }
+
+    public class Repository<T> : IRepository<T> where T : class, new()
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public void ChangeTable(string table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public T? Find(params object?[]? keyValues)
+        {
+            return _dbSet.Find(keyValues);
+        }
+
+        public ValueTask<T?> FindAsync(params object?[]? keyValues)
+        {
+            return _dbSet.FindAsync(keyValues);
+        }
+
+        public T Insert(T entity)
+        {
+            return _dbSet.Add(entity: entity).Entity;
+        }
+
+        public async ValueTask<EntityEntry<T>> InsertAsync(T entity, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return await _dbSet.AddAsync(entity: entity, cancellationToken);
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>>? predicate = null,
+                                    Func<IQueryable<T>, IIncludableQueryable<IQueryable, object>>? include = null,
+                                    Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                    bool disableTracking = false, bool IgnoreQueryFilter = false)
+        {
+            IQueryable<T> queryable = _dbSet;
+
+            if (disableTracking)
+            {
+                queryable = queryable.AsNoTracking();
+            }
+
+            if (predicate != null)
+            {
+                queryable = queryable.Where(predicate);
+            }
+
+            include?.Invoke(queryable);
+
+            orderBy?.Invoke(queryable);
+
+            if (IgnoreQueryFilter)
+                queryable = queryable.IgnoreQueryFilters();
+
+            return queryable;
+        }
+
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? predicate = null,
+                                               Func<IQueryable<T>, IIncludableQueryable<IQueryable, object>>? include = null,
+                                               Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                               bool disableTracking = false, bool IgnoreQueryFilter = false)
+        {
+            IQueryable<T> queryable = _dbSet;
+
+            if (disableTracking)
+            {
+                queryable = queryable.AsNoTracking();
+            }
+
+            if (predicate != null)
+            {
+                queryable = queryable.Where(predicate);
+            }
+
+            include?.Invoke(queryable);
+
+            orderBy?.Invoke(queryable);
+
+            if (IgnoreQueryFilter)
+                queryable = queryable.IgnoreQueryFilters();
+
+            return await queryable.ToListAsync();
+        }
+
+        public virtual TResult? GetFirstOrDefaultSelector<TResult>(Expression<Func<T, TResult>>? selector = null,
+                                                                  Expression<Func<T, bool>>? predicate = null,
+                                                                  Func<IQueryable<T>, IIncludableQueryable<IQueryable, object>>? include = null,
+                                                                  Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                                                  bool disableTracking = false, bool IgnoreQueryFilter = false)
+        {
+            IQueryable<T> queryable = _dbSet;
+
+            if (disableTracking)
+            {
+                queryable = queryable.AsNoTracking();
+            }
+
+            if (predicate != null)
+            {
+                queryable = queryable.Where(predicate);
+            }
+
+            include?.Invoke(queryable);
+
+            orderBy?.Invoke(queryable);
+
+            if (IgnoreQueryFilter)
+                queryable = queryable.IgnoreQueryFilters();
+
+            if (selector != null)
+            {
+                return queryable.Select(selector).FirstOrDefault();
+            }
+
+            return (TResult?)(object?)queryable.FirstOrDefault();
+        }
+
+        public virtual async Task<TResult?> GetFirstOrDefaultSelectorAsync<TResult>(Expression<Func<T, TResult>>? selector = null,
+                                                                   Expression<Func<T, bool>>? predicate = null,
+                                                                   Func<IQueryable<T>, IIncludableQueryable<IQueryable, object>>? include = null,
+                                                                   Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                                                   bool disableTracking = false,
+                                                                   bool IgnoreQueryFilter = false)
+        {
+            IQueryable<T> queryable = _dbSet;
+
+            if (disableTracking)
+            {
+                queryable = queryable.AsNoTracking();
+            }
+
+            if (predicate != null)
+            {
+                queryable = queryable.Where(predicate);
+            }
+
+            include?.Invoke(queryable);
+
+            orderBy?.Invoke(queryable);
+
+            if (IgnoreQueryFilter)
+                queryable = queryable.IgnoreQueryFilters();
+
+            if (selector != null)
+            {
+                return await queryable.Select(selector).FirstOrDefaultAsync();
+            }
+
+            return (TResult?)(object?)await queryable.FirstOrDefaultAsync();
+        }
+
+        public T? GetFirstOrDefault(Expression<Func<T, bool>>? predicate = null,
+                                            Func<IQueryable<T>, IIncludableQueryable<IQueryable, object>>? include = null,
+                                            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                            bool disableTracking = false, bool IgnoreQueryFilter = false)
+        {
+            IQueryable<T> queryable = _dbSet;
+
+            if (disableTracking)
+            {
+                queryable = queryable.AsNoTracking();
+            }
+
+            if (predicate != null)
+            {
+                queryable = queryable.Where(predicate);
+            }
+
+            include?.Invoke(queryable);
+
+            orderBy?.Invoke(queryable);
+
+            if (IgnoreQueryFilter)
+                queryable = queryable.IgnoreQueryFilters();
+
+            return queryable.FirstOrDefault();
+        }
+
+        public async Task<T?> GetFirstOrDefaultAsync(Expression<Func<T, bool>>? predicate = null,
+                                                              Func<IQueryable<T>, IIncludableQueryable<IQueryable, object>>? include = null,
+                                                              Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                                              bool disableTracking = false, bool IgnoreQueryFilter = false)
+        {
+            IQueryable<T> queryable = _dbSet;
+
+            if (disableTracking)
+            {
+                queryable = queryable.AsNoTracking();
+            }
+
+            if (predicate != null)
+            {
+                queryable = queryable.Where(predicate);
+            }
+
+            include?.Invoke(queryable);
+
+            orderBy?.Invoke(queryable);
+
+            if (IgnoreQueryFilter)
+                queryable = queryable.IgnoreQueryFilters();
+
+            return await queryable.FirstOrDefaultAsync();
+        }
+
+        public Task<PaginatedList<T>> GetToPageListAsync(Expression<Func<T, bool>>? predicate = null,
+                                                         Func<IQueryable<T>, IIncludableQueryable<IQueryable, object>>? include = null,
+                                                         Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                                         bool disableTracking = false, bool IgnoreQueryFilter = false,
+                                                         int pageSize = 10, int pageIndex = 1)
+        {
+            IQueryable<T> queryable = _dbSet;
+
+            if (disableTracking)
+            {
+                queryable = queryable.AsNoTracking();
+            }
+
+            if (predicate != null)
+            {
+                queryable = queryable.Where(predicate);
+            }
+
+            include?.Invoke(queryable);
+
+            orderBy?.Invoke(queryable);
+
+            if (IgnoreQueryFilter)
+                queryable = queryable.IgnoreQueryFilters();
+
+            return queryable.ToPageListAsync(pageIndex, pageSize);
+        }
+
+        public PaginatedList<T> GetToPageList(Expression<Func<T, bool>>? predicate = null,
+                                              Func<IQueryable<T>, IIncludableQueryable<IQueryable, object>>? include = null,
+                                              Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                              bool disableTracking = false, bool IgnoreQueryFilter = false,
+                                              int pageSize = 10, int pageIndex = 1)
+        {
+            IQueryable<T> queryable = _dbSet;
+
+            if (disableTracking)
+            {
+                queryable = queryable.AsNoTracking();
+            }
+
+            if (predicate != null)
+            {
+                queryable = queryable.Where(predicate);
+            }
+
+            include?.Invoke(queryable);
+
+            orderBy?.Invoke(queryable);
+
+            if (IgnoreQueryFilter)
+                queryable = queryable.IgnoreQueryFilters();
+
+            return queryable.ToPageList(pageIndex, pageSize);
+        }
+
+        public void Attach(T entity)
+        {
+            _dbSet.Attach(entity);
+        }
+
+        public void UpdateFields(T entity, params Expression<Func<T, object>>[] updatedProperties)
+        {
+            // Attach thực thể với Id
+            _dbSet.Attach(entity);
+
+            // Đánh dấu các trường cần cập nhật
+            foreach (var property in updatedProperties)
+            {
+                var propertyName = ((MemberExpression)property.Body).Member.Name;
+                _context.Entry(entity).Property(propertyName).IsModified = true;
+            }
+        }
+
+        public string GetPropertyName<TProperty>(Expression<Func<T, TProperty>> propertyExpression)
+        {
+            if (propertyExpression.Body is MemberExpression memberExpression)
+            {
+                return memberExpression.Member.Name;
+            }
+
+            throw new ArgumentException("Expression must be a member expression", nameof(propertyExpression));
+        }
+    }
+}
---
+++ UnitofWork/UnitOfWork.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UnitofWork
+{
+    public interface IUnitOfWork<TContext> : IDisposable where TContext : DbContext
+    {
+        IRepository<T> GetRepository<T>() where T : class, new();
+        Task<int> SaveChangesAsync();
+        TContext Context { get; }
+    }
+    public class UnitOfWork<TContext> : IUnitOfWork<TContext> where TContext : DbContext
+    {
+        private bool _disposed = false;
+        private readonly TContext _context;
+        private readonly Dictionary<string, object> _repositories;
+
+        public TContext Context => _context;
+
+        public UnitOfWork(TContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _repositories = [];
+        }
+
+        public IRepository<T> GetRepository<T>() where T : class, new()
+        {
+            var type = typeof(T).Name;
+
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(Repository<>);
+                var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(T)), _context) ?? throw new InvalidOperationException($"Failed to create an instance of {repositoryType.MakeGenericType(typeof(T))}");
+                _repositories.Add(type, repositoryInstance);
+            }
+
+            return (IRepository<T>)_repositories[type];
+
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+
+        // Dispose Pattern
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context?.Dispose();
+                }
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        //Destructor
+        ~UnitOfWork() => Dispose(false);
+    }
+}
